dbda67154efc142bbb4e426111a4070335263d40
lucienlmp <lucienlmp@hotmail.com>
2024-04-17 16:57:43 +0100

    initial commit

---
+++ keyword_search.py
@@ -0,0 +1,39 @@
+
+import requests
+
+r = requests.get("https://www.london.gov.uk/sitemap.xml?page=1")
+
+print(dir(r))
+
+# import requests
+# from bs4 import BeautifulSoup
+
+# sitemap_url = "https://www.london.gov.uk/sitemap.xml?page=1"
+# response = requests.get(sitemap_url)
+# sitemap_content = response.text
+
+# soup = BeautifulSoup(sitemap_content, "xml")
+# urls = [loc.text for loc in soup.find_all("loc")]
+
+# search_keyword = input("Enter the keyword or phrase to search for: ")
+
+
+# total_count = 0
+# found_urls = []
+
+# for url in urls:
+#     response = requests.get(url)
+#     webpage_content = response.text
+
+#     soup = BeautifulSoup(webpage_content, "html.parser")
+#     body_content = soup.body.get_text()
+
+#     count = body_content.lower().count(search_keyword.lower())
+#     if count > 0:
+#         total_count += count
+#         found_urls.append(url)
+
+# print(f"Total occurrences of '{search_keyword}': {total_count}")
+# print("URLs where the keyword was found:")
+# for url in found_urls:
+#        print(url)
