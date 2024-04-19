6b71e9d38fb16be154054e66fd1766286006fd19
lucienlmp <lucienlmp@hotmail.com>
2024-04-19 22:56:25 +0100

    apply try except block to unoptimised file

--- keyword_search.py
+++ keyword_search.py
@@ -10,7 +10,14 @@ def search_keyword_in_webpage(content, keyword):
 
 # Main block of code to perform the task
 sitemap_url = "https://www.london.gov.uk/sitemap.xml?page=1"
-sitemap_content = requests.get(sitemap_url, timeout=10).text
+
+try:
+    sitemap_content = requests.get(sitemap_url, timeout=10).text
+except requests.exceptions.RequestException as e:
+    print(f"Error fetching sitemap: {e}")
+    exit()
+      
+
 soup = BeautifulSoup(sitemap_content, "xml")
 urls = [loc.text for loc in soup.find_all("loc")]
 urls = urls[0:10]
@@ -21,16 +28,20 @@ total_count = 0
 
 
 for url in urls:
-    response = requests.get(url)
-    webpage_content = response.text
+    try:
+        response = requests.get(url)
+        webpage_content = response.text
 
-    soup = BeautifulSoup(webpage_content, "html.parser")
-    body_content = soup.get_text()
+        soup = BeautifulSoup(webpage_content, "html.parser")
+        body_content = soup.get_text()
 
-    occurrences = search_keyword_in_webpage(body_content, keyword)
-    if occurrences > 0:
-                results[url] = occurrences
-                total_count += occurrences
+        occurrences = search_keyword_in_webpage(body_content, keyword)
+        if occurrences > 0:
+                    results[url] = occurrences
+                    total_count += occurrences
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching URL {url}: {e}")
    
 
 # Output report
